d1a2cbec399ec06e99f14ef1d5e18063e024832d
kokka <kokka@10.0.0.195>
2018-06-24 14:31:49 -0400

    hello

---
+++ Example/src/day2/Assignement_operators.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class Assignement_operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int firstNumber,secondNumber;
+		firstNumber=90;
+		secondNumber=89;
+		if(firstNumber > secondNumber) {
+			System.out.println("first number is greater");
+			}
+		else if (firstNumber <secondNumber){
+			System.out.println("second number is greater");}
+		else {
+					System.out.println("both are equal");
+			
+		}
+			
+		}}
+	
+
+
---
+++ Example/src/day2/Variables.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int firstNumber;
+		firstNumber = 50;
+		long secondNumber = 50l;
+		byte firstValue = 84;
+		short myShortNum = 343;
+		char mychar = '6';
+		boolean status = 6 > 7;
+		String myStr="name";
+		System.out.println("name ::" + myStr + (firstNumber + secondNumber));
+		System.out.println("first number ::" + firstNumber);
+		System.out.println("second number ::" + secondNumber);
+		System.out.println("first value ::" + firstValue);
+		System.out.println("my Short Num ::" + myShortNum);
+		System.out.println("status ::" + status);
+
+	}
+
+}
